0f4ae52ef1fa5197a57e66f542d3461f651a97da
Sandro Loch <es.loch@gmail.com>
2020-04-30 16:41:56 -0300

    Added spaces variable into automl file

--- automl.py
+++ automl.py
@@ -1,7 +1,10 @@
 import time
 
+
+spaces = ( "  " )
+
 for position in range(50):
     with open('drawings/marc.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position}{object_}', end='\r')
+    print(f'{spaces * position}{object_}', end='\r')
     time.sleep(.1)
